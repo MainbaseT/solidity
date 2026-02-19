388b75d20b6b3ff0ef81bbf6823a575017111075
Kamil Śliwak <kamil.sliwak@codepoets.it>
2026-02-19 15:44:37 +0100

    Fix whitespace in some abiEncodeDecode tests

--- test/libsolidity/semanticTests/abicoder/abi_decode_offset_overflow_in_array_2.sol
+++ test/libsolidity/semanticTests/abicoder/abi_decode_offset_overflow_in_array_2.sol
@@ -6,21 +6,21 @@ contract Test {
 	}
 	function withinArray() public pure returns (uint) {
 		uint[] memory before = new uint[](1);
-		bytes memory corrupt = abi.encode(uint(32),
-										  uint(2));
+		bytes memory corrupt = abi.encode(
+			uint(32),
+			uint(2)
+		);
 		MemoryTuple memory afterCorrupt;
 		before[0] = 123456;
-		/*
-		  As above, but in this case we are adding to:
-		  0x80 + 64 (before) + 32 (length of corrupt) + 32 (offset) + 32 (field pointer)
-		  giving MAX_UINT - 96
-		*/
+
+		// As above, but in this case we are adding to:
+		// 0x80 + 64 (before) + 32 (length of corrupt) + 32 (offset) + 32 (field pointer)
+		// giving MAX_UINT - 96
 		afterCorrupt.field1 = uint(0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff60);
 		afterCorrupt.field2 = uint(0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff60);
 		uint[][] memory decoded = abi.decode(corrupt, (uint[][]));
-		/*
-		  Will return 123456 * 2, AKA before has been copied twice
-		 */
+
+		//Will return 123456 * 2, AKA before has been copied twice
 		return decoded[0][0] + decoded[1][0];
 	}
 }
